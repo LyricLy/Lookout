c90b4f59b47f16087eb018937ee8ca51890fe307
Christina Hanson <christinahansondesu@gmail.com>
2025-10-19 03:34:30 +1300

    ...

--- migrations/v2-aliases.sql
+++ migrations/v2-aliases.sql
@@ -3,14 +3,4 @@ CREATE TABLE Aliases (
     dst TEXT NOT NULL
 );
 
--- manual for now
-INSERT INTO Aliases VALUES ("Jeg11", "Jona1");
-INSERT INTO Aliases VALUES ("Machete", "Jona1");
-INSERT INTO Aliases VALUES ("townofsalem69", "Obviously");
-INSERT INTO Aliases VALUES ("Danman34682", "Waffleslice");
-INSERT INTO Aliases VALUES ("MobilePhoin", "Phoin");
-INSERT INTO Aliases VALUES ("skasineALT", "skasine");
-INSERT INTO Aliases VALUES ("vallourn", "Jinq");
-INSERT INTO Aliases VALUES ("Emberweed", "Carnifloran");
-
 CREATE INDEX bls_by_account ON Blacklists (account_name);
